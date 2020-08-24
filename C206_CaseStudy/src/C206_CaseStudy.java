110a0a41b8e46d376be7e7e1305bc65dab43024e
Nuurulhidayah <19040408@myrp.edu.sg>
2020-08-24 14:40:38 +0800

    addItem - hidayah

--- C206_CaseStudy/src/C206_CaseStudy.java
+++ C206_CaseStudy/src/C206_CaseStudy.java
@@ -1,9 +1,10 @@
+import java.util.ArrayList;
 
 public class C206_CaseStudy {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+		ArrayList<Item> itemList = new ArrayList<Item>();
 	}
 
 }
